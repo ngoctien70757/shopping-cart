1f7b907966211e3a172bfcfa270d103afd45a07c
ngoctien70757 <ngoctien70757@gmail.com>
2022-03-14 15:22:01 +0700

    upload file

---
+++ shopping-cart/src/components/Home.js
@@ -0,0 +1,9 @@
+import { CartState } from '../Context/Context'
+
+const Home = () => {
+    const { state } = CartState()
+    console.log(state)
+    return <div>Home</div>
+}
+
+export default Home
